ea3f8683aa8c62528bf93e28ab65108c89342875
SVI3 <82755620+SVI3@users.noreply.github.com>
2021-05-13 18:47:10 +0200

    Add files via upload

---
+++ strings 2/main.py
@@ -0,0 +1,30 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+
+player_one = 'Ruud Gullit '
+player_two = 'Marco van Basten '
+
+goal_0 = 32
+goal_1 = 54
+
+scorers = player_one + str(goal_0) + ', ' + player_two + str(goal_1)
+print(scorers)
+
+report = f'{player_one}scored in the {goal_0}nd minute\n{player_two}scored in the {goal_1}th minute'
+
+print(report)
+
+player = 'Igor Belanov'
+
+first_name = player[:(player.find(' '))]
+
+last_name_len = len(player)-player.find(' ')-1
+
+name_short = player[0] + '.' + player[(player.find(' ')):]
+
+chant = ((first_name + '! ')*(len(first_name)-1))+(first_name + '!')
+
+good_chant = chant[-1] != ' '
